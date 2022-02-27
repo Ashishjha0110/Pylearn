a47e1bb7cf97e5bb315313ebb9a8ade3936784ec
Ashish Jha <ashishjhaeee@gmail.com>
2022-02-27 14:48:49 +0000

    update few math fun

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #Fundamental Data types
-print (type (2+4))
-print (type (2/4)) # Output will not be rounded 
+# print (type (2+4))
+# print (type (2/4)) # Output will not be rounded 
 
-## Output will be rounded down to the integer.
-print (5 // 4)
-print (5 % 4) # modulus operator returns the remainder.
-print (3**2)
+# ## Output will be rounded down to the integer.
+# print (5 // 4)
+# print (5 % 4) # modulus operator returns the remainder.
+# print (3**2)
+
+print (round(3.9))
+print (abs(-12))
+print (bin(6))
