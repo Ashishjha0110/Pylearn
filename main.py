5310099f3e731b6da0fe8e8c4426831a2d9617e4
Ashish Jha <ashishjhaeee@gmail.com>
2022-02-27 15:25:31 +0000

    String and its concatenation

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-#agumented assignment operator
-value= 5
-value += 3 #here in place of + it can be any arithematic operator. The operator always comes left of the = sign.
-print(value)
+#String and its functions
+## Facts: We can use single as well as double quotes 
+## for the string representation in Python.
+## There is another way to represent this, which is the long string. It uses triple quotes. 
+##Example 1
+# username = 'Ashish'
+# password= "Jha0194"
+# long_String = ''' 
+# WOW
+# 0 0
+# ( )
+# '''
+# print (long_String)
+
+##Adding two strings using the '+' operator/ String Concatenation
+##Example 2
+First_String= 'Life goes'
+Second_String= 'On On'
+Final_String= First_String+' '+Second_String
+print (Final_String)
+
+
+
+
+
+
