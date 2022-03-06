6b23dc25132acb4140226da204e5d4b19bc06488
Ashish Jha <ashishjhaeee@gmail.com>
2022-03-06 14:32:35 +0000

    Create a program to check the password

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-#String and its functions
-## Facts: We can use single as well as double quotes 
-## for the string representation in Python.
-## There is another way to represent this, which is the long string. It uses triple quotes. 
-##Example 1
-# username = 'Ashish'
-# password= "Jha0194"
-# long_String = ''' 
-# WOW
-# 0 0
-# ( )
-# '''
-# print (long_String)
+##Create a program to check the password 
+username = input ('Enter the desired username:')
+password = input ('Enter the password:')
+paslen = '*' * len (password)
 
-##Adding two strings using the '+' operator/ String Concatenation
-##Example 2
-First_String= 'Life goes'
-Second_String= 'On On'
-Final_String= First_String+' '+Second_String
-print (Final_String)
+#print('Hi your username is:' +username + 'and your password is :' +password +'which is '+paslen +'long')
+print(f'{username},your password,{paslen},is {len(password)}long')
 
 
 
