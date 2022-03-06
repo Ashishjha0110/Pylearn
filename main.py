ef7a4a00eee56546e55b0ce135cba33558776977
Ashish Jha <ashishjhaeee@gmail.com>
2022-03-06 14:57:35 +0000

    list

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-##Create a program to check the password 
-username = input ('Enter the desired username:')
-password = input ('Enter the password:')
-paslen = '*' * len (password)
+##List Slicing
+#1. List are mutable
+amazon_cart =['notebook','candies','jwellery','googles']
+amazon_cart[0]='laptop' #changing the value at index 0
+# if we want to copy the list then it should be done in this way
+new_cart= amazon_cart[:]
+print(amazon_cart)
+#print(amazon_cart[1:3])
+#print(amazon_cart[0::])
 
-#print('Hi your username is:' +username + 'and your password is :' +password +'which is '+paslen +'long')
-print(f'{username},your password,{paslen},is {len(password)}long')
-
-
-
+##At this point as we have not assigned any slicing
+## so the changes at index 0 happens in the amazon cart stored in the memory location.
+new_cart=amazon_cart
+new_cart[0]='gum'
+print(new_cart)
+print(amazon_cart)
 
 
 
