f5ce1c1c607e9387bec86f7243f68ffa1079289a
Ashish Jha <ashishjhaeee@gmail.com>
2022-03-12 19:53:31 +0000

    List methods 1.4 1.sort method 2. sorted function 3.reverse

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #List methods Part 1.2
 New_list = ['a','b','c','d','e','f','b','b']
+New = ['a','b','c','d','e','f','b','b','a','c']
 
-# INDEX method: This method is used to get the index of a particular object.
-#Syntax: .index(value,start,stop)
-print(New_list.index('d',0,4))
+# SORT method: This method does not give any output. it is simply use to sort the value of the list. 
+# There is function named "SORTED(iterable,key,reverse)" but that function returns the value.
+New_list.sort()
+print(sorted(New))
+print(New_list)
 
-#in keyword
-print('d' in New_list)
+# "reverse" method is use to reverse the list without sorting it.
 
-#COUNT method: This method is used to count the number of times an object occurs in a given list.
-
-print (New_list.count('b'))
+New_list.reverse()
+print (New_list)
