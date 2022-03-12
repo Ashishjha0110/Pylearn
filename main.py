f8145a5efbd7469bc4c0986531744af76c0f894f
Ashish Jha <ashishjhaeee@gmail.com>
2022-03-12 18:55:07 +0000

    List Methods 1.1 having append,insert and Extend

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-##Matrix
-#Matrix is 2D list or multidimensional list
-matrix= [
-  [1,3,8],
-  [3,5,7],
-  [4,6,9] 
-]
+#List methods
+New_list = [1,2,3,4,5]
 
-print(matrix[0][2])
+#1. Adding a new object to the exciting list
+# Append Method: this method changes the list in place 
+# i.e. it means it does not give out a new list, it just adds the value to the existing list. 
+#Example1
+New_list.append (6)
+new= New_list
+print (New_list)
+print (new)
 
+#INSERT Method: It is used to insert the object at a particular index/location in the existing list. 
+#Example 2
+#Syntax: .insert(index,value)
+New_list.insert(3,7)
+I_new=New_list
+print (I_new)
 
+#EXTEND Menthod: This method is use to extend the existing list and it does not create a new copy with modified value. 
+#Note: this method takes "Iterable" instead of normal object
+New_list.extend([8])
+Ex_new=New_list
+print (Ex_new)
