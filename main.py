f51c0d40bace80986d009813a84c704597aa80ea
Ashish Jha <ashishjhaeee@gmail.com>
2022-03-13 12:59:01 +0000

    Dictionary and few of its method 1. get 2.dict

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-#List unpacking
-# This basically means that we can assign the list vslues a specific variable 
-#Example
-a,b,c,*others,d= [1,2,3,4,5,6,7,8,9,10]
-print (a)
-print (b)
-print (c)
-print (others)
-print (d) # the d variable is assigned to the last object of the list.
+#Dictionary
+#Dictoionary keys has to be immutsble that is why we cannot use list as the keys for a dictionary.
+dictionary= {
+'a': [1,2,3],
+'b': 'hello',
+'c': 4  
+}
+
+print (dictionary['a'][1])
+
+#Dictionary Methods
+#1. get method
+#Syntax: .get
+
+user={
+'weapons':[1,2,3,4],
+'greet' : 'hi',
+'age' : 23  
+  
+}
+print(user.get('age',26))# this line simply mean that if the age key does not have a value assigned to it then assign a the default value 26 to it but if it has then print the value which is there in the dictionary.
+
+#dict method is use to create a dictionary
+user2 = dict(name='Ashish')
+print(user2)
